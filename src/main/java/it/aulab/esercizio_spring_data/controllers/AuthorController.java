8b8e28833ade609e1c40a1a20dc7127c40466004
Tommaso Mancuso <tjmancuso@virgilio.it>
2026-04-24 18:09:15 +0200

    esercizio finito

--- src/main/java/it/aulab/esercizio_spring_data/controllers/AuthorController.java
+++ src/main/java/it/aulab/esercizio_spring_data/controllers/AuthorController.java
@@ -3,13 +3,18 @@ package it.aulab.esercizio_spring_data.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import it.aulab.esercizio_spring_data.models.Post;
 import it.aulab.esercizio_spring_data.models.Author;
 import it.aulab.esercizio_spring_data.repositories.AuthorRepository;
 
@@ -34,4 +39,25 @@ public class AuthorController {
     public Author createAuthor(@RequestBody Author author){
         return authorRepository.save(author);
     }
+
+    @PutMapping("{id}")
+    public Author updateAuthor(@PathVariable("id") Long id, @RequestBody Author author){
+        author.setId(id);
+        return authorRepository.save(author);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteAuthor(@PathVariable("id") Long id){
+        if (authorRepository.existsById(id)) {
+            Author author = authorRepository.findById(id).get();
+            List<Post> authorPosts = author.getPosts();
+            for(Post post : authorPosts){
+                post.setAuthor(null);
+            }
+            authorRepository.deleteById(id);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID dell'autore non trovato!");
+        }
+    }
+
 }
